21eae7d566dd0a739299177c5a3a4cde8b945f31
Ignacio <ignacio@sigma.local>
2016-03-28 22:30:19 -0300

    Archivos día 1 laboratorio de mecánica

--- practica1/ODE_ej2a.c
+++ practica1/ODE_ej2a.c
@@ -5,17 +5,18 @@
 
 // define parametros para usar en el sistem en todo el codigo
 struct Par{
-  double  mu, omega;
+  double  gamma, omega;
 } aa;
 
 
 // Ecuaciones del sistema
 void ecuaciones(int n, double v[], double dv[], double t){
   double x,y;
-  x=v[0];/*y=v[1];*/
+  x=v[0];
+  y=v[1];
 
-  dv[0]= x+10/(1+(x-1)*(x-1)*aa.omega)-0.1*exp(aa.mu*x);
-/*  dv[1]= x + a*y*(x*x + y*y);*/
+  dv[0]= y;
+  dv[1]= -2*aa.gamma*y-aa.omega*aa.omega*x;
   
   return;
 }
@@ -29,34 +30,34 @@ int main(){
   double v[2],t,dt,t_pre,t_max;
 
   //Archivo de salida
-  ptr=fopen("ej1.dat","w");
+  ptr=fopen("ej2a.dat","w");
 
   dt=0.01;
   t_max=2;
 
   //Condiciones Iniciales
+  v[0] = 0.5;
+  v[1] = 0.1;
   
-  for (i=0;i<=10;i++) {
-  for (j=1;i<=20;i++) {
-    v[0]=0.5*i;
-    aa.omega = 0.005*j;
-    aa.mu = 0.002*j;
-    /*v[1]=0;*/
-				
+  for (j=1;j<=5;j++) {
+   for (i=1;j<=10;j++) {
+    aa.gamma=j;
+    aa.omega=10*i;
+ 
     t=0.;
 					
     while(t<t_max){
       //Integra las ecuaciones utilizando el metodo de Runge Kutta
-      rk4(ecuaciones,v,1,t,dt);
+      rk4(ecuaciones,v,2,t,dt);
     
       //Imprime la integracion
-      fprintf(ptr,"%lg\t%lg\n",t,v[0]);
+      fprintf(ptr,"%lg\t%lg\t%lg\n",t,v[0],v[1]);
 
       t+=dt;
     }
           fprintf(ptr,"\n");
   }
-  }
+}
   
 fclose(ptr);
   return(0);
